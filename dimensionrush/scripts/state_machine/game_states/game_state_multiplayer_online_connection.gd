b90c2063e194980add4bdb89ed0fcba883de607a
Noah Levin <noah.levin.coding@gmail.com>
2024-09-24 19:41:34 +0200

    Add select game mode

--- dimensionrush/scripts/state_machine/game_states/game_state_multiplayer_online_connection.gd
+++ dimensionrush/scripts/state_machine/game_states/game_state_multiplayer_online_connection.gd
@@ -40,9 +40,11 @@ func _on_client_button_up():
 	GameManager.multiplayer_mode = GameManager.MULTIPLAYER_MODES.CLIENT
 	peer.create_client(host_ip, 135)
 	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_peer_connected)
 
 func _on_peer_connected(id = 1):
 	print("Peer connected with id: " + str(id))
+	state_transition.emit(self, "SelectGameMode")
 
 
 func _on_back_button_up():
@@ -53,6 +55,7 @@ func close_connection():
 	if multiplayer.peer_connected.is_connected(_on_peer_connected):
 		multiplayer.peer_connected.disconnect(_on_peer_connected)
 	
-	multiplayer.multiplayer_peer.close()
-	multiplayer.multiplayer_peer = null
+	if multiplayer.has_multiplayer_peer():
+		multiplayer.multiplayer_peer.close()
+		multiplayer.multiplayer_peer = null
 	
