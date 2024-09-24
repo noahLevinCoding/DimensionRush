fd46f9427f119b3fba0dad948a76cfb5cf48d429
Noah Levin <noah.levin.coding@gmail.com>
2024-09-24 23:04:28 +0200

    Add rpcs

--- dimensionrush/scripts/state_machine/game_states/game_state_select_game_mode.gd
+++ dimensionrush/scripts/state_machine/game_states/game_state_select_game_mode.gd
@@ -25,23 +25,27 @@ func _on_time_button_up():
 	GameManager.game_mode = GameManager.GAME_MODES.TIME
 	
 	if GameManager.player_mode == GameManager.PLAYER_MODES.MULTIPLAYER_ONLINE and GameManager.multiplayer_mode == GameManager.MULTIPLAYER_MODES.SERVER:
-		rpc_on_time_button_up.rpc()
+		select_game_mode_on_time_button_up.rpc()
 	
 	state_transition.emit(self, "InitGame")
 	
 @rpc
-func rpc_on_time_button_up():
+func select_game_mode_on_time_button_up():
 	GameManager.game_mode = GameManager.GAME_MODES.TIME
 	
 	state_transition.emit(self, "InitGame")
 
-#@rpc
+
 func _on_distance_button_up():
 	GameManager.game_mode = GameManager.GAME_MODES.DISTANCE
 	
 	if GameManager.player_mode == GameManager.PLAYER_MODES.MULTIPLAYER_ONLINE and GameManager.multiplayer_mode == GameManager.MULTIPLAYER_MODES.SERVER:
-		#_on_distance_button_up.rpc()
-		pass
+		select_game_mode_on_distance_button_up.rpc()
 		
 	state_transition.emit(self, "InitGame")
 	
+@rpc
+func select_game_mode_on_distance_button_up():
+	GameManager.game_mode = GameManager.GAME_MODES.DISTANCE
+	
+	state_transition.emit(self, "InitGame")
