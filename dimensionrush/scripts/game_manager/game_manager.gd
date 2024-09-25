41b5e31b0217071b89a6141eff4aefb7c52f0d41
Noah Levin <noah.levin.coding@gmail.com>
2024-09-25 19:04:56 +0200

    Add init game

--- dimensionrush/scripts/game_manager/game_manager.gd
+++ dimensionrush/scripts/game_manager/game_manager.gd
@@ -1,12 +1,21 @@
 extends Node
 
-enum PLAYER_MODES {SINGLEPLAYER, MULTIPLAYER_LOCAL, MULTIPLAYER_ONLINE}
-enum MULTIPLAYER_MODES {CLIENT, SERVER}
-enum GAME_MODES {TIME, DISTANCE}
+enum PLAYER_MODES {NONE, SINGLEPLAYER, MULTIPLAYER_LOCAL, MULTIPLAYER_ONLINE}
+enum MULTIPLAYER_MODES {NONE, CLIENT, SERVER}
+enum GAME_MODES {NONE, TIME, DISTANCE}
 
-var player_mode : PLAYER_MODES
-var multiplayer_mode : MULTIPLAYER_MODES
-var game_mode : GAME_MODES
+var player_mode : PLAYER_MODES = PLAYER_MODES.NONE
+var multiplayer_mode : MULTIPLAYER_MODES = MULTIPLAYER_MODES.NONE
+var game_mode : GAME_MODES = GAME_MODES.NONE
 
 var first_player_controls_resource 	: PlayerControlsResource = null
 var second_player_controls_resource : PlayerControlsResource = null
+
+func is_online_multiplayer() -> bool:
+	return player_mode == PLAYER_MODES.MULTIPLAYER_ONLINE
+	
+func is_server() -> bool:
+	return multiplayer_mode == MULTIPLAYER_MODES.SERVER
+	
+func is_client() -> bool:
+	return multiplayer_mode == MULTIPLAYER_MODES.CLIENT
--- dimensionrush/scripts/state_machine/game_states/game_state_init_game.gd
+++ dimensionrush/scripts/state_machine/game_states/game_state_init_game.gd
@@ -1,9 +1,34 @@
 class_name GameStateInitGame
 extends State
 
+@export var start_button : Button
 
 func enter():
+	resetUI()
+	init_game_on_online_ready.rpc()
 	visible = true
+	
 	
 func exit():
 	visible = false
+
+
+@rpc
+func init_game_on_online_ready():
+	start_button.disabled = false
+
+@rpc 
+func init_game_on_start_button_up():
+	state_transition.emit(self, "Playing")
+
+func _on_start_button_up():
+	init_game_on_start_button_up.rpc()
+	state_transition.emit(self, "Playing")
+
+
+func _on_back_button_up():
+	state_transition.emit(self, "SelectGameMode")
+
+func resetUI():
+	if GameManager.is_online_multiplayer():
+		start_button.disabled = true
