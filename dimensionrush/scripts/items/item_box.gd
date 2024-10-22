2dd2a1fe08002784e372f421e8a5c7af37b60639
Noah Eli <noah.levin.coding@gmail.com>
2024-10-22 20:16:27 +0200

    Fix multiplayer authority

--- dimensionrush/scripts/items/item_box.gd
+++ dimensionrush/scripts/items/item_box.gd
@@ -32,6 +32,6 @@ func randomize_item():
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		if not GameManager.is_online_multiplayer() or is_multiplayer_authority():
+		if not GameManager.is_online_multiplayer() or body.is_multiplayer_authority():
 			if body.inventory.add_item(item.type):
 				self.queue_free()
