cf02151bdd6b2f686f22dfa8a556e26024deb07d
Noah Eli <noah.levin.coding@gmail.com>
2024-11-03 17:26:02 +0100

    Add room and region memorization

--- dimensionrush/scripts/rooms/room_spawner.gd
+++ dimensionrush/scripts/rooms/room_spawner.gd
@@ -7,11 +7,17 @@ extends Node2D
 @export var end_room 	 : PackedScene 
 @export var regions : Array[RegionResource] 
 
+var already_spawned_regions_size = 3
+var already_spawned_regions : Array[RegionResource] = []
+
+var already_spawned_rooms_size = 3
+var already_spawned_rooms : Array[PackedScene] = []
+
 var regions_per_game : int = 0
 var region_per_game : int = 0
 
-var rooms_per_region_max : int = 6
-var rooms_per_region_min : int = 2
+var rooms_per_region_max : int = 2
+var rooms_per_region_min : int = 6
 var rooms_per_region : int = 0
 var room_per_region : int = 0
 
@@ -53,6 +59,10 @@ func init() -> void:
 	rng.state = 500
 	
 	regions_per_game = GameManager.level_distance
+	already_spawned_regions = []
+	
+	if already_spawned_regions_size >= regions.size():
+		already_spawned_regions_size = regions.size() - 1
 	
 	instantiate_rooms()
 
@@ -64,8 +74,18 @@ func instantiate_rooms() -> void:
 	add_child(current_room)
 	
 	region_index = rng.randi() % regions.size()
+	while regions[region_index] in already_spawned_regions:
+		region_index = (region_index + 1) % regions.size()
+		
+	already_spawned_regions.push_back(regions[region_index])
+	if already_spawned_regions.size() > already_spawned_regions_size:
+		already_spawned_regions.pop_front()
+	
+	#TODO
+	already_spawned_rooms_size = regions[region_index].rooms.size() / 2 
+	already_spawned_rooms = []
+	
 	rooms_per_region = rng.randi_range(rooms_per_region_min, rooms_per_region_max)
-	print("rooms per region: " + str(rooms_per_region))
 	room_per_region = 1
 	region_per_game = 1
 	
@@ -91,7 +111,14 @@ func _on_spawn_trigger_entered() -> void:
 
 func spawn_room_in_region():
 	var next_room_index = rng.randi() % regions[region_index].rooms.size()
-	var next_room_scene = regions[region_index].rooms[next_room_index]
+	while regions[region_index].rooms[next_room_index] in already_spawned_rooms:
+		next_room_index = (next_room_index + 1) % regions[region_index].rooms.size()
+		
+	var next_room_scene = regions[region_index].rooms[next_room_index]	
+	
+	already_spawned_rooms.push_back(next_room_scene)
+	if already_spawned_rooms.size() > already_spawned_rooms_size:
+		already_spawned_rooms.pop_front()
 	
 	next_room = next_room_scene.instantiate()
 	next_room.position.x = current_room.width + current_room.position.x
@@ -108,8 +135,21 @@ func spawn_end_room_in_region():
 		
 func spawn_start_room_in_new_region():
 	region_index = rng.randi() % regions.size()
+	while regions[region_index] in already_spawned_regions:
+		region_index = (region_index + 1) % regions.size()
+		
+	already_spawned_regions.push_back(regions[region_index])
+	if already_spawned_regions.size() > already_spawned_regions_size:
+		already_spawned_regions.pop_front()
+		
+		#TODO
+	already_spawned_rooms_size = regions[region_index].size() / 2 
+	already_spawned_rooms = []
+		
+	print("Region index: " + str(region_index))
+
+		
 	rooms_per_region = rng.randi_range(rooms_per_region_min, rooms_per_region_max)
-	print("rooms per region: " + str(rooms_per_region))
 	
 	region_per_game += 1
 	if region_per_game > regions_per_game:
