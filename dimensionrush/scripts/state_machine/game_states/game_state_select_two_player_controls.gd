617160e8e4bedc0ff92db7f5cc8942ab7939eff7
Noah Eli <noah.levin.coding@gmail.com>
2025-01-28 16:49:31 +0100

    Add button sounds

--- dimensionrush/scripts/state_machine/game_states/game_state_select_two_player_controls.gd
+++ dimensionrush/scripts/state_machine/game_states/game_state_select_two_player_controls.gd
@@ -3,9 +3,14 @@ extends State
 
 @export var player_controls_resources : Array[PlayerControlsResource]
 
-@export var player_controls_resource_id_label_1 : Label
-@export var player_controls_resource_id_label_2 : Label
-@export var next_button : Button
+@export var keyboard_icon_node_1 : Node2D
+@export var controller_icon_node_1 : Node2D
+
+@export var keyboard_icon_node_2 : Node2D
+@export var controller_icon_node_2 : Node2D
+
+@export var next_button : CustomTextureButton
+@export var next_label : CustomLabel
 
 var selected_player_controls_resource_1 : PlayerControlsResource = null
 var selected_player_controls_resource_2 : PlayerControlsResource = null
@@ -31,14 +36,26 @@ func _unhandled_input(_event: InputEvent):
 		if is_action_of_player_controls_resource_just_pressed(player_controls_resource) and not is_player_controls_resource_already_in_use(player_controls_resource):
 			if is_first_players_choice:
 				selected_player_controls_resource_1 = player_controls_resource
-				player_controls_resource_id_label_1.text = str(player_controls_resource.id)
+			
+				match player_controls_resource.type:
+					PlayerControlsResource.INPUT_DEVICE_TYPE.KEYBOARD:
+						keyboard_icon_node_1.visible = true
+					PlayerControlsResource.INPUT_DEVICE_TYPE.CONTROLLER:
+						controller_icon_node_1.visible = true
+			
 				is_first_players_choice = false
 			else:
 				selected_player_controls_resource_2 = player_controls_resource
-				player_controls_resource_id_label_2.text = str(player_controls_resource.id)
+				match player_controls_resource.type:
+					PlayerControlsResource.INPUT_DEVICE_TYPE.KEYBOARD:
+						keyboard_icon_node_2.visible = true
+					PlayerControlsResource.INPUT_DEVICE_TYPE.CONTROLLER:
+						controller_icon_node_2.visible = true
+			
 				next_button.disabled = false
+				next_label.disabled = false
+				
 		
-
 func is_action_of_player_controls_resource_just_pressed(player_controls_resource: PlayerControlsResource) -> bool:
 	var left_just_pressed 	= Input.is_action_just_pressed(player_controls_resource.left)
 	var right_just_pressed 	= Input.is_action_just_pressed(player_controls_resource.right)
@@ -54,9 +71,14 @@ func is_player_controls_resource_already_in_use(player_controls_resource : Playe
 		return player_controls_resource == selected_player_controls_resource_1
 
 func reset():
-	player_controls_resource_id_label_1.text = "0"
-	player_controls_resource_id_label_2.text = "0"
+
+	keyboard_icon_node_1.visible = false
+	controller_icon_node_1.visible = false
+	keyboard_icon_node_2.visible = false
+	controller_icon_node_2.visible = false
+	
 	next_button.disabled = true
+	next_label.disabled = true
 	selected_player_controls_resource_1 = null
 	selected_player_controls_resource_2 = null
 	is_first_players_choice = true
--- dimensionrush/scripts/ui/button/text_texture_button.gd
+++ dimensionrush/scripts/ui/button/text_texture_button.gd
@@ -1,15 +1,52 @@
 class_name CustomTextureButton
 extends TextureButton
 
+@export var hover_sound : AudioStreamMP3 = load("res://assets/sfx/button/click-buttons-ui-menu-sounds-effects-button-7-203601.mp3")
+@export var click_sound : AudioStreamMP3 = load("res://assets/sfx/button/button_09-190435.mp3")
+
+
+var audio_stream_player_hover : AudioStreamPlayer = null
+var audio_stream_player_click : AudioStreamPlayer = null
+
+var changed_visibility = false
+
 @export var label : CustomLabel
 
 func _ready() -> void:
+	visibility_changed.connect(_on_visiblity_changed)
+	
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
+	
+	pressed.connect(_on_pressed)
+	
+	audio_stream_player_hover = AudioStreamPlayer.new()
+	add_child(audio_stream_player_hover)
+	if hover_sound != null:
+		audio_stream_player_hover.stream = hover_sound
+		audio_stream_player_hover.volume_db = -10
+		
+	audio_stream_player_click = AudioStreamPlayer.new()
+	add_child(audio_stream_player_click)
+	if click_sound != null:
+		audio_stream_player_click.stream = click_sound
+		audio_stream_player_click.volume_db = -20
+	
+#Prevent playing sounds directly after state change	
+func _on_visiblity_changed():
+	changed_visibility = true
+	await get_tree().create_timer(0.25, true, false, true).timeout
+	changed_visibility = false
 
 func _on_mouse_entered() -> void:
 	label.hovered = true
-
+	
+	if not disabled and not changed_visibility:
+		audio_stream_player_hover.play()
+		
+func _on_pressed() -> void:
+	if not disabled:
+		audio_stream_player_click.play()
 
 func _on_mouse_exited() -> void:
 	label.hovered = false
