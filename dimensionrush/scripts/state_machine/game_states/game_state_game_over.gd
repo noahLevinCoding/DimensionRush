c480e22f1c7d554583e62a30d6cbb43b6569d5c2
Noah Eli <noah.levin.coding@gmail.com>
2025-02-02 21:20:55 +0100

    Fuck you

--- dimensionrush/scripts/state_machine/game_states/game_state_game_over.gd
+++ dimensionrush/scripts/state_machine/game_states/game_state_game_over.gd
@@ -39,7 +39,36 @@ func enter():
 func exit():
 	visible = false
 
-func _on_restart_button_up():
+@rpc("any_peer")
+func game_over_on_restart_button_up(game_seed):
+	GameManager.game_seed = game_seed
+	state_transition.emit(self, "InitGame")
+
+
+
+@rpc("any_peer")
+func game_over_on_titlescreen_button_up():
+	state_transition.emit(self, "Titlescreen")
+
+	
+@rpc("any_peer")
+func pause_on_change_game_mode_button_up():
+	state_transition.emit(self, "SelectGameMode")
+
+
+func _on_quit_button_pressed() -> void:
+	if GameManager.is_online_multiplayer():
+		game_over_on_titlescreen_button_up.rpc()
+	state_transition.emit(self, "Titlescreen")
+
+
+func _on_change_game_mode_button_pressed() -> void:
+	if GameManager.is_online_multiplayer():
+		pause_on_change_game_mode_button_up.rpc()
+	state_transition.emit(self, "SelectGameMode")
+
+
+func _on_restart_button_pressed() -> void:
 	if GameManager.auto_game_seed:
 		GameManager.game_seed = randi()
 	
@@ -47,27 +76,3 @@ func _on_restart_button_up():
 		game_over_on_restart_button_up.rpc(GameManager.game_seed)
 		
 	state_transition.emit(self, "InitGame")
-
-@rpc("any_peer")
-func game_over_on_restart_button_up(game_seed):
-	GameManager.game_seed = game_seed
-	state_transition.emit(self, "InitGame")
-
-
-func _on_titlescreen_button_up():
-	if GameManager.is_online_multiplayer():
-		game_over_on_titlescreen_button_up.rpc()
-	state_transition.emit(self, "Titlescreen")
-
-@rpc("any_peer")
-func game_over_on_titlescreen_button_up():
-	state_transition.emit(self, "Titlescreen")
-
-func _on_change_game_mode_button_up() -> void:
-	if GameManager.is_online_multiplayer():
-		pause_on_change_game_mode_button_up.rpc()
-	state_transition.emit(self, "SelectGameMode")
-	
-@rpc("any_peer")
-func pause_on_change_game_mode_button_up():
-	state_transition.emit(self, "SelectGameMode")
