7feb5040cf9a2d723f801cdab19071593a3e7458
Noah Eli <noah.levin.coding@gmail.com>
2025-02-04 17:44:00 +0100

    Add audio settings

--- dimensionrush/scripts/ui/settings.gd
+++ dimensionrush/scripts/ui/settings.gd
@@ -4,3 +4,23 @@ extends Node2D
 
 func _on_back_pressed() -> void:
 	queue_free()
+
+
+func _on_audio_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_controls_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_dimensions_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_items_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
