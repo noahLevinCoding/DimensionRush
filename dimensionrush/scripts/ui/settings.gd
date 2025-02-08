d8daf804f495f0df75ff0b71fc19bc41c755328e
Noah Eli <noah.levin.coding@gmail.com>
2025-02-08 16:30:31 +0100

    Add settings menus

--- dimensionrush/scripts/ui/settings.gd
+++ dimensionrush/scripts/ui/settings.gd
@@ -1,26 +1,40 @@
 extends Node2D
 
+@export var content_node : Control
 
+var settings_audio_scene 	  = preload("res://scenes/settings/settings_audio.tscn")
+var settings_controls_scene   = preload("res://scenes/settings/settings_controls.tscn")
+var settings_dimensions_scene = preload("res://scenes/settings/settings_dimensions.tscn")
+var settings_items_scene 	  = preload("res://scenes/settings/settings_items.tscn")
+var settings_credits_scene 	  = preload("res://scenes/settings/settings_credits.tscn")
 
 func _on_back_pressed() -> void:
 	queue_free()
 
+func clear_content() -> void:
+	for child in content_node.get_children():
+		child.queue_free()
 
 func _on_audio_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_content()
+	content_node.add_child(settings_audio_scene.instantiate())
 
 
 func _on_controls_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_content()
+	content_node.add_child(settings_controls_scene.instantiate())
 
 
 func _on_dimensions_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_content()
+	content_node.add_child(settings_dimensions_scene.instantiate())
 
 
 func _on_items_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_content()
+	content_node.add_child(settings_items_scene.instantiate())
 
 
 func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
+	clear_content()
+	content_node.add_child(settings_credits_scene.instantiate())
