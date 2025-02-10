3adcbf2af32524c048d04004f801c5c7ec01f377
Noah Eli <noah.levin.coding@gmail.com>
2025-02-10 18:47:39 +0100

    Add background music

--- dimensionrush/scripts/ui/button/options_button.gd
+++ dimensionrush/scripts/ui/button/options_button.gd
@@ -5,7 +5,10 @@ var options_scene_instance = null
 
 func _on_options_button_pressed() -> void:
 	options_scene_instance = options_scene.instantiate()
+	options_scene_instance.owner_state = owner
 	add_child(options_scene_instance)
 	options_scene_instance.global_position = Vector2.ZERO
+	
+	
 	
 	
--- dimensionrush/scripts/ui/settings.gd
+++ dimensionrush/scripts/ui/settings.gd
@@ -2,13 +2,20 @@ extends Node2D
 
 @export var content_node : Control
 
+var owner_state : State
+
 var settings_audio_scene 	  = preload("res://scenes/settings/settings_audio.tscn")
 var settings_controls_scene   = preload("res://scenes/settings/settings_controls.tscn")
 var settings_dimensions_scene = preload("res://scenes/settings/settings_dimensions.tscn")
 var settings_items_scene 	  = preload("res://scenes/settings/settings_items.tscn")
 var settings_credits_scene 	  = preload("res://scenes/settings/settings_credits.tscn")
 
+func _ready() -> void:
+	_on_audio_button_pressed()
+	#owner_state.visible = false
+
 func _on_back_pressed() -> void:
+	#owner_state.visible = true
 	queue_free()
 
 func clear_content() -> void:
